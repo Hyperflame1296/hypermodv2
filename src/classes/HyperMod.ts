7120857d0c892aa42725e7c4ae0e0049a658f149
Hyperflame1296 <jasircranshaw001@gmail.com>
2025-12-21 14:14:39 -0500

    bruh

--- src/classes/HyperMod.ts
+++ src/classes/HyperMod.ts
@@ -14,7 +14,7 @@ export class HyperMod {
     player: Player = new Player
     npsTracker: NPSTracker = new NPSTracker
     currentFile: string
-    version: string = 'v0.2.0.79'
+    version: string = 'v0.2.0.81'
     defaultSettings = {
         // MPP section
         forceInfNoteQuota: true,
